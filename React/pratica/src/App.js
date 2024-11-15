5e1b1e708558211b6dfee3a391250b1f04fc2bc9
Michel-S2 <freitasmichel1414@gmail.com>
2024-11-15 11:40:33 -0300

    done

--- React/pratica/src/App.js
+++ React/pratica/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { ListRender } from './components/ListRender';
 import { ManageData } from './components/ManageData';
+import { showUserName } from './components/showUserName';
 
 function App() {
   return (
     <div className="App">
       <ListRender></ListRender>
+      <showUserName name="Michel"/>
     </div>
   );
 }
---
+++ React/pratica/src/components/showUserName.js
@@ -0,0 +1,5 @@
+export const showUserName = (props) => {
+    return (
+        <h2>Olá familia sou o {props.name}</h2>
+    )
+}
