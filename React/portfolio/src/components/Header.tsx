f26dbdefed4112b13a29693c1e77dc715c07f4ec
michelF <freitasmichel1414@gmail.com>
2024-12-02 17:20:07 -0300

    mensagem

--- React/portfolio/src/components/Header.tsx
+++ React/portfolio/src/components/Header.tsx
@@ -1,9 +1,16 @@
+"use client"
 import "./Header.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons'
+import { faSun, faMoon, faBars, faTimes } from '@fortawesome/free-solid-svg-icons'
+import { useState } from "react";
 
 export const Header = () => {
+    const [menuOpen, setMenuOpen] = useState<boolean>(false);
     const caracter:string = '</>';
+
+    const handleMenuMobile = () => {
+        setMenuOpen(!menuOpen);
+    }
 
     return (
         <header>
@@ -11,17 +18,34 @@ export const Header = () => {
                 <div className="home">
                     <h2><span className="span">{caracter}</span> MICHEL <span>FREITAS</span></h2>
                 </div>
-                <nav>
-                    <ul>
+                <nav className="nav-menu-desk">
+                    <div className="btn-abrir" onClick={handleMenuMobile}>
+                        <FontAwesomeIcon icon={faBars} className="hamburguer-icon"/>
+                    </div>
+                    <menu className={`menu-mobile ${menuOpen ? 'mostrar' : ''}`} onClick={() => setMenuOpen(false)}>
+                        <div>
+                            <FontAwesomeIcon icon={faTimes} className="btn-fechar"/>
+                        </div>
+                        <nav className="nav-menu-mobile" >
+                            <ul className="menu">
+                                <li><a href="">Home</a></li>
+                                <li><a href="">Sobre</a></li>
+                                <li><a href="">Projetos</a></li>
+                                <li><a href="">Contato</a></li>
+                            </ul>
+                        </nav>
+                    </menu>
+                    <div className={`overlay ${menuOpen ? 'on' : ''}`} onClick={() => setMenuOpen(false)}></div>
+                    <ul className="lista-navegacao">
                         <li><a href="">Home</a></li>
                         <li><a href="">Sobre</a></li>
                         <li><a href="">Projetos</a></li>
                         <li><a href="">Contato</a></li>
                     </ul>
-                    <input type="checkbox" id="check" className="checkbox"/>
+                    {/* <input type="checkbox" id="check" className="checkbox"/> */}
                     <label htmlFor="check">
-                        <FontAwesomeIcon icon={faSun} className="sunIcon"/>
                         <FontAwesomeIcon icon={faMoon} className="moonIcon"/>
+                        <FontAwesomeIcon icon={faSun} className="sunIcon"/>
                         <div className="ball"></div>
                     </label>
                 </nav>
--- React/portfolio/src/components/Sobre2.tsx
+++ React/portfolio/src/components/Sobre2.tsx
@@ -9,13 +9,9 @@ export const Sobre = () => {
                     {/* <img src="/images/michel.jpg" alt="" /> */}
                 </div>
                 <article>
-                    <p>👨‍💻 Desenvolvedor Front-End</p>
-                    <p>🎓 Cursando Engenharia de Software</p>
-                    <p>📚 Conhecimentos em ReactJs, TailwindCss e TypeScript</p>
-                    <p>🚀 Buscando uma oportunidade para atuar como desenvolvedor</p>
-                    {/* <p>Me chamo <span>Michel</span>, tenho 22 anos, sou estudante de <span>Engenharia de Software</span>, e focado em <span>Desenvolvimento Web Front-End</span></p>
+                    <p>Me chamo <span>Michel</span>, tenho 22 anos, sou estudante de <span>Engenharia de Software</span>, e focado em <span>Desenvolvimento Web Front-End</span></p>
                     <p>Estou em busca da minha primeira oportunidade no mundo da programação, possuo os conhecimento das seguintes tecnologias: <span>ReactJs, TailwindCss, TypeScript</span></p>
-                    <p>Estou em constante evolução de estudo e sempre querendo aprender mais </p> */}
+                    <p>Estou em constante evolução de estudo e sempre querendo aprender mais </p>
                 </article>
             </section>
         </section>
