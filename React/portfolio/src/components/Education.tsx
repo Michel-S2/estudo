c15e7a887329c143bbc97f7c9eaf5ca7561c175e
Michel-S2 <freitasmichel1414@gmail.com>
2024-12-04 12:53:39 -0300

    done

--- React/portfolio/src/components/Education.tsx
+++ React/portfolio/src/components/Education.tsx
@@ -13,7 +13,7 @@ export const Education = () => {
                     <div className="timeline-content">
                         <small className='timeline-date'><FontAwesomeIcon icon={faCalendarDays}/> 2018 - 2020</small>
                         <h3>Ensino Médio (Cândido José de Godói)</h3>
-                        <p>Estudei nesse periodo no colégio Cândido José de Godói em Porto Alegre</p>
+                        <p>Durante esse periodo estudei e me formei no colegio Godói em Porto Alegre.</p>
                     </div>
                 </div>
 
@@ -22,7 +22,7 @@ export const Education = () => {
                     <div className="timeline-content">
                         <small className='timeline-date'><FontAwesomeIcon icon={faCalendarDays}/> 2021 - 2023</small>
                         <h3>Curso Técnico em Informática para Internet (Alcides Maya)</h3>
-                        <p>No curso tecnico em informática para internet na escola Alcides Maya foi onde eu tive contato pela primeira vez com o mundo da tecnologia</p>
+                        <p>Nesse curso que eu tive o primeiro contato com o mundo da tecnologia e decidi que viraria desenvolvedor.</p>
                     </div>
                 </div>
 
@@ -31,7 +31,7 @@ export const Education = () => {
                     <div className="timeline-content">
                         <small className='timeline-date'><FontAwesomeIcon icon={faCalendarDays}/> 2024 </small>
                         <h3>Curso de Programação B7WEB (Online)</h3>
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad tenetur aperiam, quod eaque id explicabo, modi obcaecati impedit officia maxime animi. Consequatur molestiae laboriosam reprehenderit labore alias! Ea, similique alias.</p>
+                        <p>Curso que estudo diariamente e está fazendo eu aprender diversas linguagens de programação, com diversos projetos práticos. No momento já estudei Html/Css, Javascript, Git/GitHub, Typescript, Tailwindcss e ReactJs.</p>
                     </div>
                 </div>
 
@@ -40,7 +40,7 @@ export const Education = () => {
                     <div className="timeline-content">
                         <small className='timeline-date'><FontAwesomeIcon icon={faCalendarDays}/> 2024 - 2029</small>
                         <h3>Bacharelado Engenharia de Software (Anhanguera)</h3>
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad tenetur aperiam, quod eaque id explicabo, modi obcaecati impedit officia maxime animi. Consequatur molestiae laboriosam reprehenderit labore alias! Ea, similique alias.</p>
+                        <p>Momento que decidi fazer parte dessa área e me matriculei em um curso superior de tecnologia para me especializar ainda mais!</p>
                     </div>
                 </div>
             </section>
